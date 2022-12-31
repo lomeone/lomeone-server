b697812a330b2645b488dd00e599fc6f7c17ecfc
comstering <coms1101@naver.com>
2023-01-01 03:32:53 +0900

    ♻️ fix: Fix db index name

--- domain/src/main/kotlin/io/github/comstering/user/entity/User.kt
+++ domain/src/main/kotlin/io/github/comstering/user/entity/User.kt
@@ -17,7 +17,7 @@ import javax.persistence.Table
 
 
 @Entity
-@Table(indexes = [Index(name = "idx_user_fireStoreId", columnList = "firebaseUserToken", unique = true)])
+@Table(indexes = [Index(name = "idx_user_firebaseUserToken", columnList = "firebaseUserToken", unique = true)])
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

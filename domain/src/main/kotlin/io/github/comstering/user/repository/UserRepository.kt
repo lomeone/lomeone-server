efec170e7bf1f5e984de4ccf02f82079da89f5ef
comstering <coms1101@naver.com>
2023-01-01 21:18:57 +0900

    ✨ feat: Add find by firebase user token function

--- domain/src/main/kotlin/io/github/comstering/user/repository/UserRepository.kt
+++ domain/src/main/kotlin/io/github/comstering/user/repository/UserRepository.kt
@@ -3,4 +3,6 @@ package io.github.comstering.user.repository
 import io.github.comstering.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByFirebaseUserToken(firebaseUserToken: String): User?
+}

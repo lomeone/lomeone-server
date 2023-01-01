917d3733def1502d1fb3015f5da4002fbe547a68
comstering <coms1101@naver.com>
2023-01-01 21:19:59 +0900

    ✨ feat: Implement create user service

--- service/src/main/kotlin/io/github/comstering/user/CreateUserService.kt
+++ service/src/main/kotlin/io/github/comstering/user/CreateUserService.kt
@@ -1,7 +1,11 @@
 package io.github.comstering.user
 
+import io.github.comstering.user.entity.AccountType
+import io.github.comstering.user.entity.Email
+import io.github.comstering.user.entity.User
 import io.github.comstering.user.repository.UserRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 import javax.validation.constraints.NotBlank
 
@@ -9,6 +13,42 @@ import javax.validation.constraints.NotBlank
 class CreateUserService(
     private val userRepository: UserRepository
 ) {
+    @Transactional
+    fun execute(request: CreateUserServiceRequest): CreateUserServiceResponse {
+        ensureUserNotExists(request.firebaseUserToken)
+
+        val user = saveUser(request)
+
+        return CreateUserServiceResponse(
+            id = user.id!!,
+            firebaseUserToken = user.firebaseUserToken,
+            name = user.name,
+            nickname = user.nickname,
+            email = user.email.value,
+            birthday = user.birthday,
+            accountType = user.accountType.name
+        )
+    }
+
+    private fun ensureUserNotExists(firebaseUserToken: String) {
+        userRepository.findByFirebaseUserToken(firebaseUserToken) != null
+            && throw Exception("User already exists")
+    }
+
+    private fun saveUser(request: CreateUserServiceRequest): User {
+        val user = userRepository.save(
+            User(
+                firebaseUserToken = request.firebaseUserToken,
+                name = request.name,
+                nickname = request.nickname,
+                email = Email(request.email),
+                birthday = request.birthday,
+                accountType = AccountType.valueOf(request.accountType)
+            )
+        )
+        user.id ?: throw Exception("User create failed")
+        return user
+    }
 }
 
 data class CreateUserServiceRequest(

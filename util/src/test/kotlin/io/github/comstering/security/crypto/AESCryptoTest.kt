1e7aeb4a201e8a3629e006cb1bff4f39ba1d3701
comstering <coms1101@naver.com>
2023-01-15 23:43:35 +0900

    ✅ test: Add AES crypto test

---
+++ util/src/test/kotlin/io/github/comstering/security/crypto/AESCryptoTest.kt
@@ -0,0 +1,15 @@
+package io.github.comstering.security.crypto
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+class AESCryptoTest : FreeSpec({
+    "암호화된 문자열을 복호화할 수 있다." - {
+        val crypto = AESCrypto("1234567890abcdef")
+        val encrypted = crypto.encrypt("test")
+        val decrypted = crypto.decrypt(encrypted)
+        encrypted shouldNotBe "test"
+        decrypted shouldBe "test"
+    }
+})

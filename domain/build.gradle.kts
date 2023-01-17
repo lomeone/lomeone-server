feb8f5574b0be4e9a1cbaac561c45e169f9b3432
comstering <coms1101@naver.com>
2023-01-18 01:36:56 +0900

    ➕ perf: Add all open plugin

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,12 @@
 group = rootProject.group
 version = rootProject.version
 
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
 dependencies {
     implementation(project(":util"))
 }

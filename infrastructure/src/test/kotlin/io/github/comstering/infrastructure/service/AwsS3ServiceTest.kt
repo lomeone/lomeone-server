ddfd3ea875c1cf32c8b38a645b3ce38444d9abaf
comstering <coms1101@naver.com>
2023-02-08 00:04:04 +0900

    🧪 test: Add Aws S3 file upload failing test

--- infrastructure/src/test/kotlin/io/github/comstering/infrastructure/service/AwsS3ServiceTest.kt
+++ infrastructure/src/test/kotlin/io/github/comstering/infrastructure/service/AwsS3ServiceTest.kt
@@ -38,4 +38,24 @@ class AwsS3ServiceTest : BehaviorSpec({
             }
         }
     }
+
+    Given("파일 리스트의 파일 중 이미지 파일이 아닌 파일이 있으면") {
+        val multipartFile = mockk<MultipartFile>()
+        every { multipartFile.originalFilename } returns "test.pdf"
+        every { multipartFile.size } returns 1024
+        every { multipartFile.contentType } returns "application/pdf"
+        every { multipartFile.inputStream } returns mockk()
+
+        val multipartFiles = listOf(multipartFile)
+
+        When("파일 리스트를 업로드할 때") {
+            every { amazonS3.putObject(any()) } returns mockk()
+            every { amazonS3.getUrl(any(), any()) } returns mockk()
+            Then("이미지파일이 아니라는 예외가 발생해서 파일이 업로드되지 않는다") {
+                shouldThrow<Exception> {
+                    awsS3Service.uploadImages(multipartFiles)
+                }
+            }
+        }
+    }
 })

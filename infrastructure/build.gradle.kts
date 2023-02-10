eb70c73250b9c8b2f51cbab8b986705b4e8c67b3
comstering <coms1101@naver.com>
2023-02-10 23:51:29 +0900

    ➕ build: Add dependency that secret manager jdbc

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -2,12 +2,14 @@ group = rootProject.group
 version = rootProject.version
 
 val springCloudAWSVersion: String by rootProject
+val awsSecretsMangerJDBCVersion: String by rootProject
 
 dependencies {
     implementation(project(":domain"))
 
     implementation("org.springframework.cloud:spring-cloud-starter-aws:$springCloudAWSVersion")
     implementation("org.springframework.cloud:spring-cloud-starter-aws-secrets-manager-config:$springCloudAWSVersion")
+    implementation("com.amazonaws.secretsmanager:aws-secretsmanager-jdbc:$awsSecretsMangerJDBCVersion")
 }
 
 tasks.getByName<Test>("test") {

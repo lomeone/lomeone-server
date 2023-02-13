04104a1012ede00910af58d87176aef4f78b7426
comstering <coms1101@naver.com>
2023-02-14 00:11:44 +0900

    ♻️ refactor: Refactor create photo usecase

--- domain/src/main/kotlin/io/github/comstering/domain/memory/usecase/CreatePhoto.kt
+++ domain/src/main/kotlin/io/github/comstering/domain/memory/usecase/CreatePhoto.kt
@@ -1,36 +1,43 @@
 package io.github.comstering.domain.memory.usecase
 
 import io.github.comstering.domain.memory.entity.Photo
-import io.github.comstering.domain.memory.entity.Post
 import io.github.comstering.domain.memory.repository.PhotoRepository
+import io.github.comstering.domain.memory.repository.PostRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import javax.validation.constraints.NotBlank
 
 @Service
 class CreatePhoto(
-    private val photoRepository: PhotoRepository
+    private val photoRepository: PhotoRepository,
+    private val postRepository: PostRepository
 ) {
     fun execute(request: CreatePhotoRequest): CreatePhotoResponse {
-        val (post, url, isMain) = request
+        val (postId, url, isMain) = request
 
-        val photo = Photo(
-            post = post,
-            url = url,
-            isMain = isMain
+        val post = getPost(postId)
+
+        val photo = photoRepository.save(
+            Photo(
+                post = post,
+                url = url,
+                isMain = isMain
+            )
         )
-
-        val savedPhoto = photoRepository.save(photo)
 
         return CreatePhotoResponse(
-            id = savedPhoto.id,
-            url = savedPhoto.url,
-            isMain = savedPhoto.isMain
+            id = photo.id,
+            url = photo.url,
+            isMain = photo.isMain
         )
     }
+
+    private fun getPost(postId: Long) =
+        postRepository.findByIdOrNull(postId) ?: throw Exception("Post not found")
 }
 
 data class CreatePhotoRequest(
-    val post: Post,
+    val postId: Long,
     @field:NotBlank val url: String,
     val isMain: Boolean
 )

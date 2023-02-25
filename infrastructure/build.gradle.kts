1ee18e67018cc28cdbf589706e122c347c128435
comstering <coms1101@naver.com>
2023-02-26 08:33:53 +0900

    ➕ build: Add postgresql jdbc

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -3,13 +3,19 @@ version = rootProject.version
 
 val springCloudAWSVersion: String by rootProject
 val awsSecretsMangerJDBCVersion: String by rootProject
+val postgresqlVersion: String by rootProject
 
 dependencies {
     implementation(project(":domain"))
 
+    // AWS
     implementation("org.springframework.cloud:spring-cloud-starter-aws:$springCloudAWSVersion")
     implementation("org.springframework.cloud:spring-cloud-starter-aws-secrets-manager-config:$springCloudAWSVersion")
     implementation("com.amazonaws.secretsmanager:aws-secretsmanager-jdbc:$awsSecretsMangerJDBCVersion")
+
+    // JDBC Driver
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
 }
 
 tasks.getByName<Test>("test") {

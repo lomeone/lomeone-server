79309400dfb12649ecccc7a736728b68442f7ac2
comstering <coms1101@naver.com>
2023-03-02 00:09:12 +0900

    ✨ feat: Add update post adapter classes

--- domain/src/main/kotlin/com/lomeone/domain/memory/usecase/UpdatePost.kt
+++ domain/src/main/kotlin/com/lomeone/domain/memory/usecase/UpdatePost.kt
@@ -2,3 +2,21 @@ package com.lomeone.domain.memory.usecase
 
 class UpdatePost {
 }
+
+data class UpdatePostRequest(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val visibility: Boolean,
+    val placeName: String,
+    val placeAddress: String
+)
+
+data class UpdatePostResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val visibility: Boolean,
+    val placeName: String,
+    val placeAddress: String
+)

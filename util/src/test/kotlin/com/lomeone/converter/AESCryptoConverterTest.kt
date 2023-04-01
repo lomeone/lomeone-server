f0e5dd160118088a6809baa65120d6d0f0f6459d
comstering <coms1101@naver.com>
2023-04-01 12:43:52 +0900

    🚚 refactor: Rename to AESCrypto converter

--- util/src/test/kotlin/com/lomeone/converter/AESCryptoConverterTest.kt
+++ util/src/test/kotlin/com/lomeone/converter/AESCryptoConverterTest.kt
@@ -4,21 +4,21 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
-class CryptoConverterTest : FreeSpec({
-    val cryptoConverter = CryptoConverter()
+class AESCryptoConverterTest : FreeSpec({
+    val converter = AESCryptoConverter()
 
     "convertToDatabaseColumn" - {
         "암호화하여 저장한다" {
             val attribute = "test"
-            val dbData = cryptoConverter.convertToDatabaseColumn(attribute)
+            val dbData = converter.convertToDatabaseColumn(attribute)
             dbData shouldNotBe "test"
         }
     }
     "convertToEntityAttribute" - {
         "복호화하여 가져온다" {
             val attribute = "test"
-            val dbData = cryptoConverter.convertToDatabaseColumn(attribute)
-            val decryptedAttribute = cryptoConverter.convertToEntityAttribute(dbData)
+            val dbData = converter.convertToDatabaseColumn(attribute)
+            val decryptedAttribute = converter.convertToEntityAttribute(dbData)
             decryptedAttribute shouldBe "test"
         }
     }

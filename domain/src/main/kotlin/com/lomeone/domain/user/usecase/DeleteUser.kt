db3085df7eb359f83c87dbde90fea5c18ba8aecf
comstering <coms1101@naver.com>
2023-04-09 13:48:28 +0900

    ✨ feat: Implement usecase that deleted user

---
+++ domain/src/main/kotlin/com/lomeone/domain/user/usecase/DeleteUser.kt
@@ -0,0 +1,30 @@
+package com.lomeone.domain.user.usecase
+
+import com.lomeone.domain.user.repository.UserRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import javax.validation.constraints.NotBlank
+
+@Service
+class DeleteUser(
+    private val userRepository: UserRepository
+) {
+    @Transactional
+    fun execute(request: DeleteUserRequest): DeleteUserResponse {
+        val user = getUser(request.userToken)
+
+        user.inactive()
+
+        return DeleteUserResponse(user.userToken)
+    }
+    private fun getUser(userToken: String) =
+        userRepository.findByUserToken(userToken) ?: throw Exception("User not found")
+}
+
+data class DeleteUserRequest(
+    @field:NotBlank val userToken: String
+)
+
+data class DeleteUserResponse(
+    val userToken: String
+)

02a69e6181b87dde2e7525a385ab70faeb6747d4
comstering <coms1101@naver.com>
2023-04-15 13:45:28 +0900

    ✨ feat: Add feature uploading images in usecase that create post

--- domain/src/main/kotlin/com/lomeone/domain/post/usecase/CreatePost.kt
+++ domain/src/main/kotlin/com/lomeone/domain/post/usecase/CreatePost.kt
@@ -4,23 +4,27 @@ import com.lomeone.domain.post.entity.Photo
 import com.lomeone.domain.post.entity.Place
 import com.lomeone.domain.post.entity.Post
 import com.lomeone.domain.post.repository.PostRepository
+import com.lomeone.domain.post.service.UploadImagesService
 import com.lomeone.domain.user.entity.User
 import com.lomeone.domain.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
 import javax.validation.constraints.NotBlank
 
 @Service
 class CreatePost(
     private val postRepository: PostRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val uploadImagesService: UploadImagesService
 ) {
     @Transactional
     fun execute(request: CreatePostRequest): CreatePostResponse {
-        val (title, content, visibility, placeName, placeAddress, userToken, photoUrls) = request
+        val (title, content, visibility, placeName, placeAddress, userToken, multipartFiles) = request
 
         val user = getUser(userToken)
         val place = Place(placeName, placeAddress)
+        val photos = getPhotos(multipartFiles)
 
         val post = postRepository.save(
             Post(
@@ -29,15 +33,21 @@ class CreatePost(
                 visibility = visibility,
                 place = place,
                 user = user,
-                photos = photoUrls.map { Photo(url = it) }.toMutableList()
+                photos = photos
             )
         )
+
         return CreatePostResponse(
             id = post.id
         )
     }
     private fun getUser(userToken: String): User =
         userRepository.findByUserToken(userToken) ?: throw Exception("User not found")
+
+    private fun getPhotos(multipartFiles: List<MultipartFile>): MutableList<Photo> {
+        val photoUrls = uploadImagesService.uploadImages(multipartFiles)
+        return photoUrls.map { Photo(url = it.value) }.toMutableList()
+    }
 }
 
 data class CreatePostRequest(
@@ -47,7 +57,7 @@ data class CreatePostRequest(
     @field:NotBlank val placeName: String,
     @field:NotBlank val placeAddress: String,
     @field:NotBlank val userToken: String,
-    @field:NotBlank val photoUrls: List<String>
+    val multipartFiles: List<MultipartFile>
 )
 
 data class CreatePostResponse(

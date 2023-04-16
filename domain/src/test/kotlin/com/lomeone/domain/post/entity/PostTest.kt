b9050f83df25ff3a17157ec4547b81926df2515f
comstering <coms1101@naver.com>
2023-04-17 00:21:54 +0900

    ✅ test: Add post entity test

---
+++ domain/src/test/kotlin/com/lomeone/domain/post/entity/PostTest.kt
@@ -0,0 +1,261 @@
+package com.lomeone.domain.post.entity
+
+import com.lomeone.domain.user.entity.User
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.mockk
+
+class PostTest : FreeSpec({
+    val placeInput = Place("placeName", "placeAddress")
+    val userInput: User = mockk()
+
+    "포스트 생성할 때" - {
+        "제목과 내용이 공백이 아니어야 생성할 수 있다" {
+            val titleInput = "title"
+            val contentInput = "content"
+
+            val post = Post(
+                title = titleInput,
+                content = contentInput,
+                place = placeInput,
+                visibility = true,
+                photos = emptyList<Photo>().toMutableList(),
+                user = userInput
+            )
+
+            post.title shouldBe titleInput
+            post.content shouldBe contentInput
+            post.place shouldBe placeInput
+            post.user shouldBe userInput
+        }
+
+        "제목이 공백이면 생성할 수 없다" {
+            val titleInput = ""
+            val contentInput = "content"
+
+            shouldThrow<IllegalArgumentException> {
+                Post(
+                    title = titleInput,
+                    content = contentInput,
+                    place = placeInput,
+                    visibility = true,
+                    photos = emptyList<Photo>().toMutableList(),
+                    user = userInput
+                )
+            }
+        }
+
+        "내용이 공백이면 생성할 수 없다" {
+            val titleInput = "title"
+            val contentInput = ""
+
+            shouldThrow<IllegalArgumentException> {
+                Post(
+                    title = titleInput,
+                    content = contentInput,
+                    place = placeInput,
+                    visibility = true,
+                    photos = emptyList<Photo>().toMutableList(),
+                    user = userInput
+                )
+            }
+        }
+    }
+
+    "포스트 정보를 업데이트할 때" - {
+        val post = Post(
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        "제목과 내용이 공백이 아니어야 업데이트할 수 있다." {
+            val titleInput = "title"
+            val contentInput = "content"
+
+            post.updatePost(
+                title = titleInput,
+                content = contentInput,
+                visibility = false,
+                place = placeInput,
+            )
+
+            post.title shouldBe titleInput
+            post.content shouldBe contentInput
+            post.visibility shouldBe false
+            post.place shouldBe placeInput
+        }
+
+        "제목이 공백이면 업데이트할 수 없다." {
+            val titleInput = ""
+            val contentInput = "content"
+
+            shouldThrow<IllegalArgumentException> {
+                post.updatePost(
+                    title = titleInput,
+                    content = contentInput,
+                    visibility = false,
+                    place = placeInput,
+                )
+            }
+        }
+
+        "내용이 공백이면 업데이트할 수 없다." {
+            val titleInput = "title"
+            val contentInput = ""
+
+            shouldThrow<IllegalArgumentException> {
+                post.updatePost(
+                    title = titleInput,
+                    content = contentInput,
+                    visibility = false,
+                    place = placeInput,
+                )
+            }
+        }
+    }
+
+    "포스트를 삭제할 수 있다" {
+        val post = Post(
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        post.delete()
+
+        post.deleted shouldBe true
+    }
+
+    "포스트를 삭제하고 다시 복원할 수 있다." {
+        val post = Post(
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        post.delete()
+        post.deleted shouldBe true
+        post.restore()
+        post.deleted shouldBe false
+    }
+
+    "포스트 id가 같으면 같은 포스트이다" {
+        val post1 = Post(
+            id = 1L,
+            title = "title1",
+            content = "content1",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        val post2 = Post(
+            id = 1L,
+            title = "title2",
+            content = "content2",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        (post1 == post2) shouldBe true
+    }
+
+    "포스트 id가 다르면 다른 포스트이다" {
+        val post1 = Post(
+            id = 1L,
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+        val post2 = Post(
+            id = 2L,
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        (post1 == post2) shouldBe false
+    }
+
+    "포스트 객체가 아니면 다른 포스트이다" {
+        val post1 = Post(
+            id = 1L,
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+        val post2 = "post2"
+
+        (post1.equals(post2)) shouldBe false
+    }
+
+    "포스트 id가 같으면 hash 값도 같다" {
+        val post1 = Post(
+            id = 1L,
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+        val post2 = Post(
+            id = 1L,
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        post1.hashCode() shouldBe post2.hashCode()
+    }
+
+    "포스트 id가 다르면 hash 값도 다르다" {
+        val post1 = Post(
+            id = 1L,
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+        val post2 = Post(
+            id = 2L,
+            title = "title",
+            content = "content",
+            place = placeInput,
+            visibility = true,
+            photos = emptyList<Photo>().toMutableList(),
+            user = userInput
+        )
+
+        post1.hashCode() shouldNotBe post2.hashCode()
+    }
+})

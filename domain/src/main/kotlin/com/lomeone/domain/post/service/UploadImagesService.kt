5bcf21ff2c7e9db5d3946ea573ceaa07df19c481
comstering <coms1101@naver.com>
2023-04-19 20:59:46 +0900

    ♻️ refactor: Rename parameter value

--- domain/src/main/kotlin/com/lomeone/domain/post/service/UploadImagesService.kt
+++ domain/src/main/kotlin/com/lomeone/domain/post/service/UploadImagesService.kt
@@ -3,7 +3,7 @@ package com.lomeone.domain.post.service
 import org.springframework.web.multipart.MultipartFile
 
 interface UploadImagesService {
-    fun uploadImages(multipartFile: List<MultipartFile>): List<Url>
+    fun uploadImages(multipartFiles: List<MultipartFile>): List<Url>
 }
 
 data class Url(val value: String)

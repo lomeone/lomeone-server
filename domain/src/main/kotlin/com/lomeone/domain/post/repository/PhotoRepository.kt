3f2ff38bace4e61556bdf2bfaba77e5a866b8f3a
comstering <coms1101@gmail.com>
2023-04-22 16:59:08 +0900

    🔥 refactor: Remove don't need code

--- domain/src/main/kotlin/com/lomeone/domain/post/repository/PhotoRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.lomeone.domain.post.repository
-
-import com.lomeone.domain.post.entity.Photo
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PhotoRepository : JpaRepository<Photo, Long>

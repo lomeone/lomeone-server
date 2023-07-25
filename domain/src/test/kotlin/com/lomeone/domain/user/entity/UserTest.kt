1c3676159acc46fd9174d5da560978da0afb4ca5
comstering <coms1101@gmail.com>
2023-07-26 00:29:06 +0900

    ✅ test: Update user test code

--- domain/src/test/kotlin/com/lomeone/domain/user/entity/UserTest.kt
+++ domain/src/test/kotlin/com/lomeone/domain/user/entity/UserTest.kt
@@ -1,175 +1,167 @@
 package com.lomeone.domain.user.entity
 
+import com.lomeone.domain.common.entity.Email
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
-import java.time.ZoneId
-import java.time.ZonedDateTime
+import java.time.LocalDate
 
-class UserTest : BehaviorSpec({
+const val USER_TOKEN = "user-token"
+
+class UserTest1 : FreeSpec({
     val defaultUser = User(
         userToken = "user1234",
         name = "name",
         nickname = "nickname",
-        email = "test@gmail.com",
-        birthday = ZonedDateTime.now(),
-        photoUrl = "https://photo.com",
-        accountType = AccountType.GOOGLE
+        email = Email("test@gmail.com"),
+        phoneNumber = "+821012345678",
+        birthday = LocalDate.now(),
+        photoUrl = "https://photo.com"
     )
 
-    Given("이름이 공백이 아니고, 닉네임도 공백이 아니고, 사진 경로도 공백이 아니고, 이메일도 형식에 맞으면") {
-        val nameInput = "John"
-        val nicknameInput = "Tomy"
-        val emailInput = "email@gmail.com"
-        val birthdayInput = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneId.of("Asia/Seoul"))
-        val photoUrlInput = "https://update.photo.com"
-        When("유저를 생성할 때") {
+    "유저를 생성할 때" {
+        "이름, 닉네임, 핸드폰번호, 사진 경로가 공백이 아니고 이메일이 형식에 맞아야 생성할 수 있다" - {
+            val nameInput = "John"
+            val nicknameInput = "Tomy"
+            val emailInput = Email("email@gmail.com")
+            val phoneNumberInput = "+821012345678"
+            val birthdayInput = LocalDate.of(2000, 1, 1)
+            val photoUrlInput = "https://update.photo.com"
+
             val user = User(
-                userToken = "user1234",
+                userToken = USER_TOKEN,
                 name = nameInput,
                 nickname = nicknameInput,
                 email = emailInput,
+                phoneNumber = phoneNumberInput,
                 birthday = birthdayInput,
-                photoUrl = "https://photo.com",
-                accountType = AccountType.GOOGLE
+                photoUrl = photoUrlInput
             )
-            Then("유저를 생성할 수 있다") {
-                user.userToken shouldBe "user1234"
-                user.name shouldBe nameInput
-                user.nickname shouldBe nicknameInput
-                user.email.value shouldBe emailInput
-                user.birthday shouldBe birthdayInput
-                user.accountType shouldBe AccountType.GOOGLE
+
+            user.userToken shouldBe USER_TOKEN
+            user.name shouldBe nameInput
+            user.nickname shouldBe nicknameInput
+            user.email shouldBe emailInput
+            user.phoneNumber shouldBe phoneNumberInput
+            user.birthday shouldBe birthdayInput
+            user.photoUrl shouldBe photoUrlInput
+        }
+
+        "이름이 공백이면 이름이 공백이라는 예외가 발생한다" - {
+            val nameInput = ""
+            shouldThrow<Exception> {
+                User(
+                    userToken = USER_TOKEN,
+                    name = nameInput,
+                    nickname = "nickname",
+                    email = Email("email@gmail.com"),
+                    phoneNumber = "+821012345678",
+                    birthday = LocalDate.now(),
+                    photoUrl = "https://photo.com"
+                )
             }
         }
-        When("유저 정보를 업데이트할 때") {
+
+        "닉네임이 공백이면 닉네임이 공백이라는 예외가 발생한다" - {
+            val nicknameInput = ""
+            shouldThrow<Exception> {
+                User(
+                    userToken = USER_TOKEN,
+                    name = "name",
+                    nickname = nicknameInput,
+                    email = Email("email@gmail.com"),
+                    phoneNumber = "+821012345678",
+                    birthday = LocalDate.now(),
+                    photoUrl = "https://photo.com"
+                )
+            }
+        }
+
+        "핸드폰 번호가 공백이면 핸드폰 번호가 공백이라는 예외가 발생한다" - {
+            val phoneNumberInput = ""
+            shouldThrow<Exception> {
+                User(
+                    userToken = USER_TOKEN,
+                    name = "name",
+                    nickname = "nickname",
+                    email = Email("email@gmail.com"),
+                    phoneNumber = phoneNumberInput,
+                    birthday = LocalDate.now(),
+                    photoUrl = "https://photo.com"
+                )
+            }
+        }
+
+        "사진 경로가 공백이면 사진 경로가 공백이라는 예외가 발생한다" - {
+            val photoUrlInput = ""
+            shouldThrow<Exception> {
+                User(
+                    userToken = USER_TOKEN,
+                    name = "name",
+                    nickname = "nickname",
+                    email = Email("email@gmail.com"),
+                    phoneNumber = "+821012345678",
+                    birthday = LocalDate.now(),
+                    photoUrl = photoUrlInput
+                )
+            }
+        }
+    }
+
+    "유저 정보를 업데이트할 때" {
+        "이름, 닉네임, 사진 경로가 공백이 아니면 유저 정보를 업데이트할 수 있다" - {
+            val nameInput = "John"
+            val nicknameInput = "Tomy"
+            val birthdayInput = LocalDate.of(2000, 1, 1)
+            val photoUrlInput = "https://update.photo.com"
 
             defaultUser.updateUserInfo(nameInput, nicknameInput, birthdayInput, photoUrlInput)
-            Then("유저 정보를 업데이트할 수 있다") {
-                defaultUser.name shouldBe nameInput
-                defaultUser.nickname shouldBe nicknameInput
-                defaultUser.birthday shouldBe birthdayInput
-                defaultUser.photoUrl shouldBe photoUrlInput
-            }
-        }
-    }
-    Given("이름이 공백이면") {
-        val nameInput = " "
-        When("유저를 생성할 때") {
-            Then("이름이 공백이라는 에외가 발생해서 유저를 생성할 수 없다") {
-                shouldThrow<Exception> {
-                    User(
-                        userToken = "user1234",
-                        name = nameInput,
-                        nickname = "nickname",
-                        email = "email@gmail.com",
-                        birthday = ZonedDateTime.now(),
-                        photoUrl = "https://photo.com",
-                        accountType = AccountType.GOOGLE
-                    )
 
-                }
+            defaultUser.name shouldBe nameInput
+            defaultUser.nickname shouldBe nicknameInput
+            defaultUser.birthday shouldBe birthdayInput
+            defaultUser.photoUrl shouldBe photoUrlInput
+        }
+
+        "이름이 공백이면 이름이 공백이라는 예외가 발생한다" - {
+            val nameInput = ""
+            shouldThrow<Exception> {
+                defaultUser.updateUserInfo(nameInput, "nickname", LocalDate.now(), "https://photo.com")
             }
         }
-        When("유저 정보를 업데이트할 때") {
-            Then("이름이 공백이라는 에외가 발생해서 유저 정보를 업데이트할 수 없다") {
-                shouldThrow<Exception> {
-                    defaultUser.updateUserInfo(nameInput, "nickname", ZonedDateTime.now(), "https://photo.com")
-                }
+
+        "닉네임이 공백이면 닉네임이 공백이라는 예외가 발생한다" - {
+            val nicknameInput = ""
+            shouldThrow<Exception> {
+                defaultUser.updateUserInfo("name", nicknameInput, LocalDate.now(), "https://photo.com")
             }
         }
-    }
-    Given("닉네임이 공백이면") {
-        val nicknameInput = ""
-        When("유저를 생성할 때") {
-            Then("닉네임이 공백이라는 예외가 발생해서 유저를 생성할 수 없다") {
-                shouldThrow<Exception> {
-                    User(
-                        userToken = "user1234",
-                        name = "name",
-                        nickname = nicknameInput,
-                        email = "email@gmail.com",
-                        birthday = ZonedDateTime.now(),
-                        photoUrl = "https://photo.com",
-                        accountType = AccountType.GOOGLE
-                    )
-                }
-            }
-        }
-        When("유저 정보를 업데이트할 때") {
-            Then("닉네임이 공백이라는 에외가 발생해서 유저 정보를 업데이트할 수 없다") {
-                shouldThrow<Exception> {
-                    defaultUser.updateUserInfo("name", nicknameInput, ZonedDateTime.now(), "https://photo.com")
-                }
+
+        "사진 경로가 공백이면 사진 경로가 공백이라는 예외가 발생한다" - {
+            val photoUrlInput = ""
+            shouldThrow<Exception> {
+                defaultUser.updateUserInfo("name", "nickname", LocalDate.now(), photoUrlInput)
             }
         }
     }
-    Given("사진 경로가 공백이면") {
-        val photoUrlInput = ""
-        When("유저를 생성할 때") {
-            Then("사진 경로가 공백이라는 예외가 발생해서 유저를 생성할 수 없다") {
-                shouldThrow<Exception> {
-                    User(
-                        userToken = "user1234",
-                        name = "name",
-                        nickname = "nickname",
-                        email = "email@gmail.com",
-                        birthday = ZonedDateTime.now(),
-                        photoUrl = photoUrlInput,
-                        accountType = AccountType.GOOGLE
-                    )
-                }
-            }
-        }
-        When("유저 정보를 업데이트할 때") {
-            Then("사진 경로가 공백이라는 예외가 발생해서 유저 정보를 업데이트할 수 없다") {
-                shouldThrow<Exception> {
-                    defaultUser.updateUserInfo("name", "nickname", ZonedDateTime.now(), photoUrlInput)
-                }
-            }
-        }
+
+    "유저는 이메일을 업데이트 할 수 있다" - {
+        val emailInput = Email("update@gamil.com")
+        defaultUser.updateEmail(emailInput)
+        defaultUser.email shouldBe emailInput
     }
-    Given("이메일이 공백이면") {
-        val emailInput = " "
-        When("유저를 생성할 때") {
-            Then("이메일이 공백이라는 예외가 발생해서 유저를 생성할 수 없다") {
-                shouldThrow<Exception> {
-                    User(
-                        userToken = "user1234",
-                        name = "name",
-                        nickname = "nickname",
-                        email = emailInput,
-                        birthday = ZonedDateTime.now(),
-                        photoUrl = "https://photo.com",
-                        accountType = AccountType.GOOGLE
-                    )
-                }
-            }
+
+    "유저는 핸드폰 번호를 업데이트할 때 " {
+        "휴대폰 번호가 공백이 아니면 업데이트 할 수 있다" - {
+            val phoneNumberInput = "+821012345678"
+            defaultUser.updatePhoneNumber(phoneNumberInput)
+            defaultUser.phoneNumber shouldBe phoneNumberInput
         }
-    }
-    Given("이메일이 형식에 맞지 않으면") {
-        val emailInput = "email"
-        When("유저를 생성할 때") {
-            Then("이메일 혁식이 맞지 않는다는 예외가 발생해서 유저를 생성할 수 없다") {
-                shouldThrow<Exception> {
-                    User(
-                        userToken = "user1234",
-                        name = "name",
-                        nickname = "nickname",
-                        email = emailInput,
-                        birthday = ZonedDateTime.now(),
-                        photoUrl = "https://photo.com",
-                        accountType = AccountType.FACEBOOK
-                    )
-                }
-            }
-        }
-    }
-    Given("유저 탈퇴 요청시") {
-        When("유저 탈퇴할 때") {
-            defaultUser.inactivate()
-            Then("탈퇴할 수 있다.") {
-                defaultUser.activated shouldBe false
+
+        "휴대폰 번호가 공백이면 휴대폰 번호가 공백이라는 예외가 발생한다" - {
+            val phoneNumberInput = ""
+            shouldThrow<Exception> {
+                defaultUser.updatePhoneNumber(phoneNumberInput)
             }
         }
     }

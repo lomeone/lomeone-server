c357ec68a97db953ee6e77c78096f94ca31fcf79
comstering <coms1101@gmail.com>
2023-09-18 22:36:59 +0900

    🔥 fix: Remove post code

--- domain/src/main/kotlin/com/lomeone/domain/post/service/UploadImagesService.kt
+++
@@ -1,9 +0,0 @@
-package com.lomeone.domain.post.service
-
-import org.springframework.web.multipart.MultipartFile
-
-interface UploadImagesService {
-    fun uploadImages(multipartFiles: List<MultipartFile>): List<Url>
-}
-
-data class Url(val value: String)

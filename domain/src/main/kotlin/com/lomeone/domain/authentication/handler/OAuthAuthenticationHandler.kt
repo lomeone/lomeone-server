8f2a3b38e4ec1b49e76ba856d2d3db67e02fecbd
comstering <coms1101@gmail.com>
2023-11-17 20:15:44 +0900

    ✨ feat: Add interface for oauth authentication

---
+++ domain/src/main/kotlin/com/lomeone/domain/authentication/handler/OAuthAuthenticationHandler.kt
@@ -0,0 +1,30 @@
+package com.lomeone.domain.authentication.handler
+
+import com.lomeone.domain.authentication.entity.AuthProvider
+
+interface OAuthAuthenticationHandler {
+    fun getOAuthProvider(): AuthProvider
+    fun getOAuthToken(request: OAuthTokensRequest): OAuthTokensResponse
+    fun getOAuthUserData(request: OAuthUserDataRequest): OAuthUserDataResponse
+}
+
+data class OAuthTokensRequest(
+    val code: String,
+    val anotherRequiredData: Map<String, String> = mapOf()
+)
+
+data class OAuthTokensResponse(
+    val accessToken: String,
+    val refreshToken: String,
+    val expiresIn: Int
+)
+
+data class OAuthUserDataRequest(
+    val accessToken: String
+)
+
+data class OAuthUserDataResponse(
+    val providerUid: String,
+    val email: String,
+    val phoneNumber: String
+)

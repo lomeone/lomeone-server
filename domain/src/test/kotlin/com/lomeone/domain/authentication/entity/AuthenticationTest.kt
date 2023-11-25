d747967c1ab3e89630431215824f88538771fcf1
comstering <coms1101@gmail.com>
2023-11-25 16:33:56 +0900

    ✅ refactor: Refactor test code

--- domain/src/test/kotlin/com/lomeone/domain/authentication/entity/AuthenticationTest.kt
+++ domain/src/test/kotlin/com/lomeone/domain/authentication/entity/AuthenticationTest.kt
@@ -10,7 +10,7 @@ class AuthenticationTest : FreeSpec({
     "계정을 생성할 때" - {
         val emailInput = Email("email@gmail.com")
         "이메일 방식이고 비밀번호가 null이 아니고 형식에 맞으면 계정이 생성된다" - {
-            val providerInput = Provider.EMAIL
+            val providerInput = AuthProvider.EMAIL
             val passwordInput = "testPassword1324@"
 
             val authentication = Authentication(
@@ -25,7 +25,7 @@ class AuthenticationTest : FreeSpec({
         }
 
         "이메일 방식이고 비밀번호가 null이 아니지만 공백이면 비밀번호가 공백이라는 예외가 발생한다" - {
-            val providerInput = Provider.EMAIL
+            val providerInput = AuthProvider.EMAIL
             val passwordInput = " "
 
             shouldThrow<Exception> {
@@ -38,7 +38,7 @@ class AuthenticationTest : FreeSpec({
         }
 
         "이메일 방식이고 비밀번호가 null이 아니지만 형식에 맞지 않으면 비밀번호가 형식에 맞지 않는다는 예외가 발생한다" - {
-            val providerInput = Provider.EMAIL
+            val providerInput = AuthProvider.EMAIL
             val passwordInput = "testPassword"
 
             shouldThrow<Exception> {
@@ -51,7 +51,7 @@ class AuthenticationTest : FreeSpec({
         }
 
         "이메일 방식인데 비밀번호가 null이면 이메일 방식은 패스워드가 필요하다는 예외가 발생한다" - {
-            val providerInput = Provider.EMAIL
+            val providerInput = AuthProvider.EMAIL
             val passwordInput = null
 
             shouldThrow<Exception> {
@@ -64,7 +64,7 @@ class AuthenticationTest : FreeSpec({
         }
 
         "이메일 방식이 아니고 비밀번호가 null이면 계정이 생성된다" - {
-            val providerInput = Provider.GOOGLE
+            val providerInput = AuthProvider.GOOGLE
             val passwordInput = null
 
             val authentication = Authentication(
@@ -79,7 +79,7 @@ class AuthenticationTest : FreeSpec({
         }
 
         "이메일 방식이 아니고 비밀번호가 null이 아니면 비밀번호가 null이어야한다는 예외가 발생한다" - {
-            val providerInput = Provider.GOOGLE
+            val providerInput = AuthProvider.GOOGLE
             val passwordInput = "testPassword1324@"
 
             shouldThrow<Exception> {
@@ -96,7 +96,7 @@ class AuthenticationTest : FreeSpec({
         val authentication = Authentication(
                 email = Email("email@gmail.com"),
                 password = "testPassword1324@",
-                provider = Provider.EMAIL
+                provider = AuthProvider.EMAIL
         )
 
         val signedInAtBefore = authentication.signedInAt
@@ -110,7 +110,7 @@ class AuthenticationTest : FreeSpec({
         val emailAuthentication = Authentication(
                 email = Email("email@gmail.com"),
                 password = "testPassword1324@",
-                provider = Provider.EMAIL
+                provider = AuthProvider.EMAIL
         )
 
         "비밀번호 형식에 맞지 않으면 비밀번호가 형식에 맞지 않는다는 예외가 발생한다" - {
@@ -132,7 +132,7 @@ class AuthenticationTest : FreeSpec({
             val googleAuthentication = Authentication(
                     email = Email("email@gmail.com"),
                     password = null,
-                    provider = Provider.GOOGLE
+                    provider = AuthProvider.GOOGLE
             )
 
             val passwordInput = "testPassword1324!"

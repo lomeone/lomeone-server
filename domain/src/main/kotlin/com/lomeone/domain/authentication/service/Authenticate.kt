923f08bb04947f651a3daf183d06e06e6672fedd
comstering <coms1101@gmail.com>
2024-03-09 00:05:31 +0900

    🔥 refactor: Remove authentication service class

--- domain/src/main/kotlin/com/lomeone/domain/authentication/service/Authenticate.kt
+++
@@ -1,7 +0,0 @@
-package com.lomeone.domain.authentication.service
-
-import com.lomeone.domain.authentication.entity.Authentication
-
-interface Authenticate<T> {
-    fun authenticate(command: T): Authentication
-}

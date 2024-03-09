f0671a4927680f0468a4c77f30cef20549f9b15d
comstering <coms1101@gmail.com>
2024-03-09 22:23:39 +0900

    ✨ feat: Add role-related entities

---
+++ domain/src/main/kotlin/com/lomeone/domain/user/entity/UserRole.kt
@@ -0,0 +1,43 @@
+package com.lomeone.domain.user.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "user_roles")
+class UserRole(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_roles_id")
+    val id: Long = 0L,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roles_id")
+    val role: Role
+)
+
+@Entity
+@Table(name = "roles")
+class Role(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "roles_id")
+    val id: Long = 0L,
+
+    @Enumerated(EnumType.STRING)
+    val roleName: RoleName
+)
+
+enum class RoleName {
+    MEMBER,
+    ADMIN
+}

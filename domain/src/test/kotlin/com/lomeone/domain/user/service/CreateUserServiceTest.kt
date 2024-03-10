3907b2be22950b9b0eca5f0be329b012dd8bc2da
comstering <coms1101@gmail.com>
2024-03-10 19:13:17 +0900

    ✅ test: Refactor test for refactored service

--- domain/src/test/kotlin/com/lomeone/domain/user/service/CreateUserServiceTest.kt
+++ domain/src/test/kotlin/com/lomeone/domain/user/service/CreateUserServiceTest.kt
@@ -1,10 +1,11 @@
 package com.lomeone.domain.user.service
 
 import com.lomeone.domain.authentication.entity.AuthProvider
-import com.lomeone.domain.authentication.entity.Authentication
-import com.lomeone.domain.common.entity.Email
+import com.lomeone.domain.authentication.service.CreateAuthenticationResult
+import com.lomeone.domain.authentication.service.CreateAuthenticationService
 import com.lomeone.domain.user.entity.User
 import com.lomeone.domain.user.repository.UserRepository
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -15,44 +16,97 @@ import java.time.LocalDate
 
 class CreateUserServiceTest : BehaviorSpec({
     val userRepository: UserRepository = mockk()
-    val createUserService = CreateUserService(userRepository)
+    val createAuthenticationService: CreateAuthenticationService = mockk()
+    val createUserService = CreateUserService(userRepository, createAuthenticationService)
 
-    val authenticationInput = Authentication(
-        email = Email("test@gmail.com"),
-        password = "testPassword1324@",
-        provider = AuthProvider.EMAIL
-    )
+    beforeTest {
+        val mockUser: User = mockk()
+        every { mockUser.userToken } returns "user-token"
+        every { userRepository.save(any()) } returns mockUser
 
-    Given("유저가 존재하지 않으면") {
-        every { userRepository.findByUserToken(any()) } returns null
+        every { createAuthenticationService.createAuthentication(any()) } returns CreateAuthenticationResult("uid1234")
+    }
+
+    Given("동일한 유저 이메일과 핸드폰 번호를 가지고 있는 유저가 없으면") {
+        every { userRepository.findByEmail(any()) } returns null
+        every { userRepository.findByPhoneNumber(any()) } returns null
+
         When("유저를 생성할 때") {
             val command = CreateUserCommand(
-                name = "name",
-                nickname = "nickname",
-                email = "email@gmail.com",
-                phoneNumber = "+821012345678",
-                birthday = LocalDate.of(2000, 1, 1),
-                authentication = authenticationInput
-            )
-
-            every { userRepository.save(any()) } returns User(
-                name = "name",
-                nickname = "nickname",
-                email = Email("email@gmail.com"),
-                phoneNumber = "+821012345678",
-                birthday = LocalDate.of(2000, 1, 1),
+                userInfo = CreateUserCommand.UserInfo(
+                    name = "name",
+                    nickname = "nickname",
+                    email = "email@gmail.com",
+                    phoneNumber = "+821012345678",
+                    birthday = LocalDate.of(2000, 1, 1),
+                ),
+                authenticationInfo = CreateUserCommand.AuthenticationInfo(
+                    email = "email@gmail.com",
+                    provider = AuthProvider.GOOGLE,
+                )
             )
 
             val result = withContext(Dispatchers.IO) {
                 createUserService.createUser(command)
             }
+
             Then("유저가 생성된다") {
-                result.id shouldBe 0L
-                result.name shouldBe command.name
-                result.nickname shouldBe command.nickname
-                result.email shouldBe command.email
-                result.phoneNumber shouldBe command.phoneNumber
-                result.birthday shouldBe command.birthday
+                result.userToken shouldBe "user-token"
+                result.authenticationUid shouldBe "uid1234"
+            }
+        }
+    }
+
+    Given("동일한 이메일을 가진 유저가 있으면") {
+        every { userRepository.findByEmail(any()) } returns mockk()
+        every { userRepository.findByPhoneNumber(any()) } returns null
+
+        When("유저를 생성할 때") {
+            val command = CreateUserCommand(
+                userInfo = CreateUserCommand.UserInfo(
+                    name = "name",
+                    nickname = "nickname",
+                    email = "email@gmail.com",
+                    phoneNumber = "+821012345678",
+                    birthday = LocalDate.of(2000, 1, 1),
+                ),
+                authenticationInfo = CreateUserCommand.AuthenticationInfo(
+                    email = "email@gmail.com",
+                    provider = AuthProvider.GOOGLE,
+                )
+            )
+
+            Then("유저가 이미 존재한다는 예외가 발생해서 유저를 생성할 수 없다") {
+                shouldThrow<Exception> {
+                    createUserService.createUser(command)
+                }
+            }
+        }
+    }
+
+    Given("동일한 핸드폰번호를 가진 유저가 있으면") {
+        every { userRepository.findByEmail(any()) } returns null
+        every { userRepository.findByPhoneNumber(any()) } returns mockk()
+
+        When("유저를 생성할 때") {
+            val command = CreateUserCommand(
+                userInfo = CreateUserCommand.UserInfo(
+                    name = "name",
+                    nickname = "nickname",
+                    email = "email@gmail.com",
+                    phoneNumber = "+821012345678",
+                    birthday = LocalDate.of(2000, 1, 1),
+                ),
+                authenticationInfo = CreateUserCommand.AuthenticationInfo(
+                    email = "email@gmail.com",
+                    provider = AuthProvider.GOOGLE,
+                )
+            )
+
+            Then("유저가 이미 존재한다는 예외가 발생해서 유저를 생성할 수 없다") {
+                shouldThrow<Exception> {
+                    createUserService.createUser(command)
+                }
             }
         }
     }

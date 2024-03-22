569969692f8863412d8ffbcf48f658f1cc5ce7b7
comstering <coms1101@gmail.com>
2024-03-22 17:09:11 +0900

    ✨ feat: Implement mutation example schema

--- application/src/main/kotlin/com/lomeone/application/graphql/HelloDataFetcher.kt
+++ application/src/main/kotlin/com/lomeone/application/graphql/HelloDataFetcher.kt
@@ -2,13 +2,19 @@ package com.lomeone.application.graphql
 
 import com.lomeone.generated.types.HelloInput
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
 
 @DgsComponent
 class HelloDataFetcher {
     @DgsQuery
-    fun hello(@InputArgument input: HelloInput): String {
-        return "${input.helloName} hello"
+    fun helloQuery(@InputArgument input: HelloInput): String {
+        return "query success ${input.helloName} hello"
+    }
+
+    @DgsMutation
+    fun helloMutation(@InputArgument input: HelloInput): String {
+        return "mutation success ${input.helloName} hello"
     }
 }

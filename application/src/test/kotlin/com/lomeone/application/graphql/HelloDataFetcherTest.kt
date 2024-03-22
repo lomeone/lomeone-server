f2e7b85d6e8a8f89d9bb219fa7b4dd925f27d2dc
comstering <coms1101@gmail.com>
2024-03-22 17:37:05 +0900

    ✅ test: Add graph mutation test example

--- application/src/test/kotlin/com/lomeone/application/graphql/HelloDataFetcherTest.kt
+++ application/src/test/kotlin/com/lomeone/application/graphql/HelloDataFetcherTest.kt
@@ -2,23 +2,41 @@ package com.lomeone.application.graphql
 
 import com.netflix.graphql.dgs.DgsQueryExecutor
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
+import com.netflix.graphql.dgs.autoconfig.DgsExtendedScalarsAutoConfiguration
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(classes = [DgsAutoConfiguration::class, HelloDataFetcher::class])
+@SpringBootTest(classes = [
+    DgsAutoConfiguration::class,
+    DgsExtendedScalarsAutoConfiguration::class,
+    HelloDataFetcher::class
+])
 class HelloDataFetcherTest(
-    private val dgsQueryExecutor: DgsQueryExecutor
+    private val dgsQueryExecutor: DgsQueryExecutor,
+    private val dgsMutationExecutor: DgsQueryExecutor
 ) : FreeSpec({
     "DataFetcher Query test Example" - {
         val result: String = dgsQueryExecutor.executeAndExtractJsonPath("""
-                {
-                    hello(input: {
+                query {
+                    helloQuery(input: {
                         helloName: "name"
                     })
                 }
-            """.trimIndent(), "data.hello")
+            """.trimIndent(), "data.helloQuery")
 
-        result shouldBe "name hello"
+        result shouldBe "query success name hello"
+    }
+
+    "DataFetcher Mutation test Example" - {
+        val result: String = dgsMutationExecutor.executeAndExtractJsonPath("""
+            mutation {
+                helloMutation(input: {
+                    helloName: "name"
+                })
+            }
+        """.trimIndent(), "data.helloMutation")
+
+        result shouldBe "mutation success name hello"
     }
 })

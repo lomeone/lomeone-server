14146724a21f16a2a8a570c61d8d93c5751bc333
comstering <coms1101@gmail.com>
2024-04-14 16:45:00 +0900

    ✅ test: Add test code

--- application/src/test/kotlin/com/lomeone/application/graphql/authentication/AuthenticationDataFetcherTest.kt
+++ application/src/test/kotlin/com/lomeone/application/graphql/authentication/AuthenticationDataFetcherTest.kt
@@ -1,13 +1,18 @@
 package com.lomeone.application.graphql.authentication
 
 import com.lomeone.LomeoneApplication
+import com.lomeone.domain.authentication.service.AuthenticateByEmail
+import com.lomeone.domain.authentication.service.JwtTokenProvider
 import com.lomeone.domain.user.repository.UserRepository
 import com.lomeone.domain.user.service.CreateUserService
+import com.lomeone.generated.types.AuthenticateResult
+import com.lomeone.generated.types.SignUpByEmailResult
 import com.netflix.graphql.dgs.DgsQueryExecutor
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
 import com.netflix.graphql.dgs.autoconfig.DgsExtendedScalarsAutoConfiguration
 import io.kotest.core.spec.style.FreeSpec
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.authentication.AuthenticationManager
 
 @SpringBootTest(classes = [
     DgsAutoConfiguration::class,
@@ -15,13 +20,16 @@ import org.springframework.boot.test.context.SpringBootTest
     LomeoneApplication::class,
     AuthenticationDataFetCher::class,
     CreateUserService::class,
-    UserRepository::class
+    UserRepository::class,
+    AuthenticateByEmail::class,
+    AuthenticationManager::class,
+    JwtTokenProvider::class
 ])
 class AuthenticationDataFetcherTest(
     private val dgsExecutor: DgsQueryExecutor,
 ) : FreeSpec({
     "이메일을 이용해서 회원가입을 할 수 있다" - {
-        val result: String = dgsExecutor.executeAndExtractJsonPath("""
+        val result: SignUpByEmailResult = dgsExecutor.executeAndExtractJsonPathAsObject("""
             mutation {
                 signUpByEmail(input: {
                     email: "test@gmail.com",
@@ -34,8 +42,27 @@ class AuthenticationDataFetcherTest(
                     uid
                 }
             }
-        """.trimIndent(), "data.signUpByEmail.uid")
+        """.trimIndent(), "data.signUpByEmail", SignUpByEmailResult::class.java)
 
-        println(result)
+        println(result.uid)
+    }
+
+    "이메일을 이용해서 로그인을 할 수 있다" - {
+        val result: AuthenticateResult = dgsExecutor.executeAndExtractJsonPathAsObject("""
+            mutation {
+                signInByEmail(input: {
+                    email: "test@gmail.com",
+                    password: "testPassword1324@"
+                }) {
+                    accessToken,
+                    refreshToken,
+                    expiresIn
+                }
+            }
+        """.trimIndent(), "data.signInByEmail", AuthenticateResult::class.java)
+
+        println(result.accessToken)
+        println(result.refreshToken)
+        println(result.expiresIn)
     }
 })

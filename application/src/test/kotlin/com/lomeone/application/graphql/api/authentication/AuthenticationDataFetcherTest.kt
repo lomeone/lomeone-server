945377119eb57c933015fae87697252533401d72
comstering <coms1101@gmail.com>
2024-08-26 00:49:39 +0900

    refactor: Fix test code

--- application/src/test/kotlin/com/lomeone/application/graphql/api/authentication/AuthenticationDataFetcherTest.kt
+++ application/src/test/kotlin/com/lomeone/application/graphql/api/authentication/AuthenticationDataFetcherTest.kt
@@ -1,12 +1,9 @@
 package com.lomeone.application.graphql.api.authentication
 
 import com.lomeone.LomeoneApplication
-import com.lomeone.application.graphql.api.authentication.AuthenticationDataFetCher
-import com.lomeone.domain.authentication.service.AuthenticateByEmail
 import com.lomeone.domain.authentication.service.JwtTokenProvider
 import com.lomeone.domain.user.repository.UserRepository
 import com.lomeone.domain.user.service.CreateUserService
-import com.lomeone.generated.types.AuthenticateResult
 import com.lomeone.generated.types.SignUpByEmailResult
 import com.netflix.graphql.dgs.DgsQueryExecutor
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
@@ -22,7 +19,6 @@ import org.springframework.security.authentication.AuthenticationManager
     AuthenticationDataFetCher::class,
     CreateUserService::class,
     UserRepository::class,
-    AuthenticateByEmail::class,
     AuthenticationManager::class,
     JwtTokenProvider::class
 ])
@@ -46,24 +42,5 @@ class AuthenticationDataFetcherTest(
         """.trimIndent(), "data.signUpByEmail", SignUpByEmailResult::class.java)
 
         println(result.uid)
-    }
-
-    "이메일을 이용해서 로그인을 할 수 있다" - {
-        val result: AuthenticateResult = dgsExecutor.executeAndExtractJsonPathAsObject("""
-            mutation {
-                signInByEmail(input: {
-                    email: "test@gmail.com",
-                    password: "testPassword1324@"
-                }) {
-                    accessToken,
-                    refreshToken,
-                    expiresIn
-                }
-            }
-        """.trimIndent(), "data.signInByEmail", AuthenticateResult::class.java)
-
-        println(result.accessToken)
-        println(result.refreshToken)
-        println(result.expiresIn)
     }
 })

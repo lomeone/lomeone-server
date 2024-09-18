acedf06e41ad6aa9de49a4ffaf8710d349a83c96
comstering <coms1101@gmail.com>
2024-09-18 22:41:12 +0900

    feat: Implement oauth authentication result processing

--- application/src/main/kotlin/com/lomeone/application/seucirty/config/SecurityConfig.kt
+++ application/src/main/kotlin/com/lomeone/application/seucirty/config/SecurityConfig.kt
@@ -3,12 +3,14 @@ package com.lomeone.application.seucirty.config
 import com.lomeone.application.seucirty.filter.EmailPasswordAuthenticationFilter
 import com.lomeone.domain.authentication.service.JwtTokenProvider
 import com.lomeone.domain.authentication.service.OAuth2UserService
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.Authentication
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -36,6 +38,13 @@ class SecurityConfig(
             .oauth2Login {
                 it.userInfoEndpoint {
                     it.userService(oAuth2UserService)
+                }
+                it.successHandler { request, response, authentication ->
+                    val tokenInfo = jwtTokenProvider.issueToken(authentication)
+                    response.addHeader("Authorization", "Bearer ${tokenInfo.accessToken}")
+                }
+                it.failureHandler { request, response, exception ->
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.message)
                 }
             }
 

d7eb3517dc6fd8be62cd0f151f86ab05cdb61668
comstering <coms1101@gmail.com>
2024-09-18 23:28:43 +0900

    refactor: Refactor oauth success process

--- application/src/main/kotlin/com/lomeone/application/seucirty/config/SecurityConfig.kt
+++ application/src/main/kotlin/com/lomeone/application/seucirty/config/SecurityConfig.kt
@@ -1,22 +1,22 @@
 package com.lomeone.application.seucirty.config
 
 import com.lomeone.application.seucirty.filter.EmailPasswordAuthenticationFilter
+import com.lomeone.application.seucirty.handler.OAuthAuthenticationSuccessHandler
 import com.lomeone.domain.authentication.service.JwtTokenProvider
 import com.lomeone.domain.authentication.service.OAuth2UserService
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.Authentication
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val oAuth2UserService: OAuth2UserService
+    private val oAuth2UserService: OAuth2UserService,
+    private val oAuthAuthenticationSuccessHandler: OAuthAuthenticationSuccessHandler
 ) {
     @Bean
     fun filterChain(
@@ -35,17 +35,12 @@ class SecurityConfig(
                 authenticationManager = authenticationConfiguration.authenticationManager,
                 jwtTokenProvider = jwtTokenProvider
             ))
-            .oauth2Login {
-                it.userInfoEndpoint {
-                    it.userService(oAuth2UserService)
-                }
-                it.successHandler { request, response, authentication ->
-                    val tokenInfo = jwtTokenProvider.issueToken(authentication)
-                    response.addHeader("Authorization", "Bearer ${tokenInfo.accessToken}")
-                }
-                it.failureHandler { request, response, exception ->
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.message)
-                }
+            .oauth2Login { oAuth2LoginConfigure ->
+                oAuth2LoginConfigure
+                    .userInfoEndpoint {
+                        it.userService(oAuth2UserService)
+                    }
+                    .successHandler(oAuthAuthenticationSuccessHandler)
             }
 
         return http.build()

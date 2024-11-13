1c7471e6fabb3fb2b69a64a429b98ae8583f8eee
comstering <coms1101@gmail.com>
2024-11-13 10:23:05 +0900

    feat: Add aws sts dependency

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -3,7 +3,7 @@ version = rootProject.version
 
 val springCloudAWSVersion: String by rootProject
 val awsSecretsMangerJDBCVersion: String by rootProject
-val postgresqlVersion: String by rootProject
+val awsStsVersion: String by rootProject
 
 dependencies {
     implementation(project(":domain"))
@@ -12,6 +12,7 @@ dependencies {
     implementation("io.awspring.cloud:spring-cloud-aws-starter:$springCloudAWSVersion")
     implementation("io.awspring.cloud:spring-cloud-aws-starter-secrets-manager:$springCloudAWSVersion")
     implementation("com.amazonaws.secretsmanager:aws-secretsmanager-jdbc:$awsSecretsMangerJDBCVersion")
+    implementation("software.amazon.awssdk:sts:$awsStsVersion")
 
     // JDBC Driver
     runtimeOnly("com.h2database:h2")

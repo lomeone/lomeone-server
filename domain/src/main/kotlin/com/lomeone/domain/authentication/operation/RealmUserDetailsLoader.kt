9f4eaffeb2c52d8b56c10a4e415e66677e5da342
comstering <comstering@gmail.com>
2025-04-15 23:44:12 +0900

    feat: Add exception for realm not found

--- domain/src/main/kotlin/com/lomeone/domain/authentication/operation/RealmUserDetailsLoader.kt
+++
@@ -1,5 +0,0 @@
-package com.lomeone.domain.authentication.operation
-
-import org.springframework.security.core.userdetails.UserDetails
-
-typealias RealmUserDetailsLoader = (realmCode: String, username: String) -> UserDetails

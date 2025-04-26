fc4e3cab6c6b98bed4948cbb3d536798a24cebe1
comstering <comstering@gmail.com>
2025-04-26 16:46:11 +0900

    refactor: Remove random string util

--- util/src/main/kotlin/com/lomeone/util/string/RandomStringUtil.kt
+++
@@ -1,7 +0,0 @@
-package com.lomeone.util.string
-
-object RandomStringUtil {
-    fun generateRandomString(charSet: Set<Char>, length: Int): String =
-        (1..length).map { charSet.random() }
-            .joinToString("")
-}

c7846925741e2e426a36673ebd4aea20b8d7c8e5
comstering <comstering@gmail.com>
2025-04-27 15:45:52 +0900

    feat: Add realm already exist exception

---
+++ domain/src/main/kotlin/com/lomeone/domain/realm/exception/RealmAlreadyExistException.kt
@@ -0,0 +1,20 @@
+package com.lomeone.domain.realm.exception
+
+import com.lomeone.eunoia.exception.ErrorCode
+import com.lomeone.eunoia.exception.EunioaException
+import com.lomeone.eunoia.exception.ExceptionCategory
+import com.lomeone.eunoia.exception.ExceptionDetail
+
+private const val MESSAGE = "Realm already exists."
+private val ERROR_CODE = ErrorCode(
+    code = "realm/already-exist",
+    exceptionCategory = ExceptionCategory.BAD_REQUEST
+)
+
+class RealmAlreadyExistException(
+    detail: Map<String, Any>
+) : EunioaException(
+    message = MESSAGE,
+    errorCode = ERROR_CODE,
+    detail = ExceptionDetail(detail)
+)

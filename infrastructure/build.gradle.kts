2dc0db96de3755e86427b61b51f6189c718b79d5
comstering <comstering@gmail.com>
2026-03-08 17:02:35 +0900

    refactor: Refactor build.gradle.kts

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,12 +1,22 @@
 plugins {
     alias(libs.plugins.kotlin.jpa)
-    alias(libs.plugins.ksp)
     // preview 끝나면 사용해보기
 //    alias(libs.plugins.aws.sdk.kotlin.dynamodb.mapper)
 }
 
 dependencies {
     implementation(project(":domain"))
+
+    // jpa
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.bundles.querydsl)
+
+    // Openfeign
+    implementation(libs.spring.cloud.starter.openfeign)
+    // Openfeign에서 spring web annotations를 그대로 사용하기 때문에 web engine을 exclude하고 import
+    implementation(libs.spring.boot.starter.web) {
+        exclude(module = "spring-boot-starter-tomcat")
+    }
 
     // aws
     implementation(libs.bundles.aws.sdk.dynamodb)
@@ -21,7 +31,8 @@ dependencies {
 
     // JDBC Driver
     runtimeOnly(libs.mysql)
+    runtimeOnly(libs.h2)
 
     // test
-    testRuntimeOnly(libs.h2)
+//    testRuntimeOnly(libs.h2)
 }

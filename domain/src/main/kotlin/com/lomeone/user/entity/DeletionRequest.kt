99bc15439d5d14e74a50b1d0d0e94748e91aa4cc
comstering <comstering@gmail.com>
2026-04-01 00:24:06 +0900

    feat: Add enumerated annotation

--- domain/src/main/kotlin/com/lomeone/user/entity/DeletionRequest.kt
+++ domain/src/main/kotlin/com/lomeone/user/entity/DeletionRequest.kt
@@ -3,6 +3,8 @@ package com.lomeone.user.entity
 import com.lomeone.common.entity.AuditEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -21,6 +23,7 @@ class DeletionRequest(
     val userToken: String,
     val reason: String
 ) : AuditEntity() {
+    @Enumerated(EnumType.STRING)
     var status: DeletionStatus = DeletionStatus.REQUEST
         protected set
 

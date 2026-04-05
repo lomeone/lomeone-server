8a4c5fff539f5138a658b977e271ed6e24a26652
comstering <comstering@gmail.com>
2026-04-05 16:30:41 +0900

    refactor: Archive util code

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -1,2 +1,9 @@
-group = rootProject.group
-version = rootProject.version
+plugins {
+    alias(libs.plugins.kotlin.jpa)
+}
+
+dependencies {
+
+    // jpa
+    implementation(libs.spring.boot.starter.data.jpa)
+}
